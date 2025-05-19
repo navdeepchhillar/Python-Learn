c07c7d4af61f167b2f764343a47a134151cd6ea2
navdeepchhillar <navdeepchhillar12@gmail.com>
2025-05-19 16:39:14 +0530

    first commit

---
+++ Crash Course.py
@@ -0,0 +1,24 @@
+# # print("Hello world")
+# # Variables
+# a=3
+# print(a)
+# a=9.0
+# print(a)
+# c=True 
+# print(c)
+
+# #operator 
+
+# num1 = 40
+# num2 = 8
+# print("num1 + num2", num1 + num2)
+# print("num1 - num2", num1 - num2)
+# print("num1 * num2", num1 * num2)
+# print("num1 / num2", num1 / num2)
+# print("num1 // num2", num1 // num2)
+# print("num1 ** num2", num1 ** num2)
+# print("num1 % num2", num1 % num2)
+
+import pandas
+df=pandas.read_csv('Task.csv')
+print(df)
